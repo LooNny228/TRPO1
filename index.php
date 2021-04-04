faa1ba7eda4de97f1a073976b127c202e1aaf324
LooNny228 <70636567+LooNny228@users.noreply.github.com>
2021-04-04 16:41:33 +0300

    Laba3

--- index.php
+++ index.php
@@ -1,38 +1,13 @@
 <?php
-class Line
-{
-    protected $x;
+namespace Laba3;
 
-    public function line_equation($a,$b)
-    {
-        if ($a != 0) {
-            return $this->x = (-$b) / $a;
-        }
-        return null;
+use Gaivoronskiy\MyLog;
 
-    }
-}
-class Quadratic extends Line implements EI
-{
-    public function quadratic_equation($a, $b, $c)
-    {
-        return ($b * $b) - 4 * $a * $c;
-    }
-
-    public function solve($a, $b, $c)
-    {
-        If ($a === 0) {
-            return parent::line_equation($b, $c);
-        }
-        $d = $this->quadratic_equation($a, $b, $c);
-        if ($d > 0) {
-            $eg = sqrt($d);
-            return $this->x = array((-$b - $eg) / (2 * $a), (-$b + $eg) / (2 * $a));
-        }
-        if ($d === 0) {
-            return $this->x = array(-$b / (2 * $a));
-        }
-        return null;
-    }
-}
-?>
+require_once "core\EquationInterface.php";
+require_once "core\LogAbstract.php";
+require_once "core\LogInterface.php";
+require_once "Gaivoronskiy\Line.php";
+require_once "Gaivoronskiy\MyLog.php";
+require_once "Gaivoronskiy\Quadratic.php";
+MyLog::log("egor");
+MyLog::write();
